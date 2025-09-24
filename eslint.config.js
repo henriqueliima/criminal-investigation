dc1d65eb3e3a1879fb5df8f1277b1fa52c400cf3
Henrique Lima <henriqueliima@users.noreply.github.com>
2025-09-24 17:17:51 -0300

    add sort imports eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -5,10 +5,18 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
